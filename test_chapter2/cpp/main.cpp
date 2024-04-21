6d381113ebaae2875038d1e2e487e12094aa2286
RusikPro <rusik.arhireev@hotmail.com>
2024-04-21 22:54:10 +0200

    Remove unnecessary chapter

--- test_chapter2/cpp/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main () {
-    std::cout << "Hello from main in test_chapter2" << std::endl;
-    return 0;
-}
--- test_chapter2/exercises/exercise.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main () {
-    std::cout << "Exercise for test_chapter2" << std::endl;
-    return 0;
-}
