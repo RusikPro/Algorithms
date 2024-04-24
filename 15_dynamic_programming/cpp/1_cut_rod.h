2fb2ae17a9aac48b6f1181c25a91e62fbce27384
RusikPro <rusik.arhireev@hotmail.com>
2024-04-24 11:44:00 +0200

    Implement memoized cut rod

--- 15_dynamic_programming/cpp/1_cut_rod.h
+++ 15_dynamic_programming/cpp/1_cut_rod.h
@@ -32,4 +32,47 @@ int cutRod ( Elements const & _p, int _n )
 
 /*----------------------------------------------------------------------------*/
 
+// Top-down with memoization
+
+int memoizedCutRodAux ( Elements const & _p, int _n, Elements & _r )
+{
+    if ( _r[ _n ] >= 0 )
+    {
+        return _r[ _n ];
+    }
+
+    int q = MINIMUM;
+
+    if ( _n == 0 )
+    {
+        q = 0;
+    }
+    else
+    {
+        for ( int i = 0; i < _n; ++i )
+        {
+            q = std::max(
+                q, _p[ i ] + memoizedCutRodAux( _p, _n - i - 1, _r )
+            );
+        }
+    }
+    _r[ _n ] = q;
+
+    return q;
+}
+
+int memoizedCutRod ( Elements const & _p, int _n )
+{
+    Elements r;
+    r.reserve( _n  + 1);
+    for ( int i = 0; i < _n + 1; ++i )
+    {
+        r.push_back( MINIMUM );
+    }
+
+    return memoizedCutRodAux( _p, _n, r );
+}
+
+/*----------------------------------------------------------------------------*/
+
 #endif // __15_DYNAMIC_PROGRAMMING_CPP_1_CUT_ROD_H__
--- 15_dynamic_programming/cpp/main.cpp
+++ 15_dynamic_programming/cpp/main.cpp
@@ -10,7 +10,7 @@ void testCutRod ()
 {
     Elements prices = { 1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
 
-    auto maximumPrice = cutRod( prices, 10 );
+    auto maximumPrice = cutRod( prices, 5 );
     std::cout << "Maximum price: " << maximumPrice << std::endl;
 
     {
@@ -36,8 +36,37 @@ void testCutRod ()
 
 /*----------------------------------------------------------------------------*/
 
+void testCutRodMemoization ()
+{
+    Elements prices = { 1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
+
+    auto maximumPrice = memoizedCutRod( prices, 4 );
+    std::cout << "Maximum price: " << maximumPrice << std::endl;
+}
+
+/*----------------------------------------------------------------------------*/
+
+void comparePerformances ()
+{
+    Elements prices = { 1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
+
+    {
+        Timer< std::milli > t( "cutRod( prices, 26 )" );
+        cutRod( prices, 26 );
+    }
+
+    {
+        Timer< std::milli > t( "memoizedCutRod( prices, 26 )" );
+        memoizedCutRod( prices, 26 );
+    }
+}
+
+/*----------------------------------------------------------------------------*/
+
 int main ()
 {
-    testCutRod();
+    // testCutRod();
+    testCutRodMemoization();
+    // comparePerformances();
     return 0;
 }
