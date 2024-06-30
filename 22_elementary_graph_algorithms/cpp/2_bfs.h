124fbefbfc4be6562f18f89cc5231de1af5bb2d9
RusikPro <rusik.arhireev@hotmail.com>
2024-06-30 16:40:56 +0200

    22 Elementary Graph Algorithms: remove redundant step in bfs algorithm

--- 22_elementary_graph_algorithms/cpp/2_bfs.h
+++ 22_elementary_graph_algorithms/cpp/2_bfs.h
@@ -9,6 +9,68 @@
 
 /*----------------------------------------------------------------------------*/
 
+/*
+struct Node
+{
+    std::list< Node * > adjacencyList;
+    Color color;
+    int distance = MAXIMUM;
+    Node * pPredecessor = nullptr;
+    char c;
+};
+*/
+
+/*----------------------------------------------------------------------------*/
+
+Node * createGraphByFigure_22_3 ()
+{
+    // Allocating array for all nodes
+    Node * adjList = new Node[ 8 ];
+
+    // Assigning letters to each vertex
+    Node * r = adjList;     r->c = 'r';
+    Node * v = adjList + 1; v->c = 'v';
+    Node * s = adjList + 2; s->c = 's';
+    Node * w = adjList + 3; w->c = 'w';
+    Node * t = adjList + 4; t->c = 't';
+    Node * x = adjList + 5; x->c = 'x';
+    Node * u = adjList + 6; u->c = 'u';
+    Node * y = adjList + 7; y->c = 'y';
+
+    // Constructing graph
+
+    r->adjacencyList.push_back( s );
+    r->adjacencyList.push_back( v );
+
+    v->adjacencyList.push_back( r );
+
+    s->adjacencyList.push_back( w );
+    s->adjacencyList.push_back( r );
+
+    w->adjacencyList.push_back( t );
+    w->adjacencyList.push_back( x );
+
+    t->adjacencyList.push_back( u );
+    t->adjacencyList.push_back( x );
+    t->adjacencyList.push_back( w );
+
+    x->adjacencyList.push_back( t );
+    x->adjacencyList.push_back( u );
+    x->adjacencyList.push_back( y );
+    x->adjacencyList.push_back( w );
+
+    u->adjacencyList.push_back( y );
+    u->adjacencyList.push_back( x );
+    u->adjacencyList.push_back( t );
+
+    y->adjacencyList.push_back( u );
+    y->adjacencyList.push_back( x );
+
+    return adjList;
+}
+
+/*----------------------------------------------------------------------------*/
+
 Node * getByLetter( Node * _graph, std::size_t _size, char _c )
 {
     for ( std::size_t i = 0; i < _size; ++i )
@@ -45,11 +107,7 @@ void bfs ( Node * _graph, std::size_t _size, Node * _s )
         Node * u = Q.front();
         Q.pop();
 
-        Node * current = getByLetter( _graph, _size, u->c );
-
-        if ( !current ) continue;
-
-        for ( Node * v: current->adjacencyList )
+        for ( Node * v: u->adjacencyList )
         {
             if ( v->color == Color::White )
             {
